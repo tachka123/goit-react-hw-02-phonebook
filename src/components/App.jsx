eba8db9628a38d863d493d3e475b34afc402e9d1
Sasha Savchuk <Sasha>
2019-12-15 21:18:12 +0200

    Эперенос в отдельный repo

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import variableCSS from './variables.module.css';
+
+import PhoneBook from './PhoneBook/PhoneBook';
+
+const App = () => {
+  return (
+    <div className={variableCSS.container}>
+      <PhoneBook />
+    </div>
+  );
+};
+
+export default App;
